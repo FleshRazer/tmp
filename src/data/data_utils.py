4c303e7a2de8bd5fa60b52b6620f1ce51e883ad3
Danil Andreev <andreevdanil35@gmail.com>
2023-10-01 16:43:11 +0300

    Add a notebook with Llama2 fine-tuning
    - Add a notebook draft
    - Fix minor issue with data_utils

--- src/data/data_utils.py
+++ src/data/data_utils.py
@@ -19,12 +19,17 @@ def download_filtered_paranmt():
     with open(filtered_paranmt_archive_filename, mode="wb") as _file:
         for chunk in response.iter_content(chunk_size=128):
             _file.write(chunk)
-    
+
+
+def unzip_filtered_paranmt():
     with zipfile.ZipFile(filtered_paranmt_archive_filename, mode="r") as _file:
         _file.extractall(os.path.dirname(filtered_paranmt_archive_filename))
 
 
 def get_filtered_paranmt_hf():
+    if not os.path.exists(filtered_paranmt_tsv_filename):
+        unzip_filtered_paranmt()
+
     df = read_csv("tmp/data/raw/filtered.tsv", sep="\t")
     dataset = Dataset.from_dict({column: df.loc[:, column].to_list() for column in df.columns})
 
